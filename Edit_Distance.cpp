1f9094fca7f56ac847c06381ffa0e899ab4f92c4
Yash <dubeycdk@gmail.com>
2024-03-24 20:46:29 +0530

    updt

---
+++ Edit_Distance.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+signed main(){
+ios::sync_with_stdio(0);
+cin.tie(0);
+string s,t;
+cin>>s>>t;
+
+    
+}
--- Minimizing_Coins.cpp
+++ Minimizing_Coins.cpp
@@ -1,16 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define int long long int
-
-signed main(){
-ios::sync_with_stdio(0);
-cin.tie(0);
-int t;
-cin>>t;
-while(t--){
-    int n,k;
-    cin>>n>>k;   
+int coinchange(vector<int> &coins, int n, int target, vector<vector<int>> &dp)
+{
+    if (n >= 0 && target == 0)
+        return 0;
+    if ((n < 0) || target < 0)
+        return INT_MAX - 1;
+    if (dp[n][target] != -1)
+        return dp[n][target];
+    if (coins[n - 1] <= target)
+    {
+        return dp[n][target] = min(1 + coinchange(coins, n, target - coins[n - 1], dp), coinchange(coins, n - 1, target, dp));
+    }
+    return dp[n][target] = coinchange(coins, n - 1, target, dp);
 }
-    
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> coins(n);
+    for (int i = 0; i < n; i++)
+        cin >> coins[i];
+    vector<int> dp(k+ 1,INT_MAX-1);
+    dp[0] = 0;
+    for (int ind = 1; ind <= n; ind++)
+    {
+        for (int target = 1; target <= k; target++)
+        {
+            if (coins[ind - 1] <= target)
+            {
+                dp[target] = min(1 + dp[target - coins[ind - 1]],dp [target]);
+            }
+        }
+    }
+
+    if (dp[k] == INT_MAX - 1)
+        cout << -1 << endl;
+    else
+        cout << dp[k] << endl;
 }
